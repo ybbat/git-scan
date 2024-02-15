5c3f531397f4f8d6904c64dc42207211a737d04e
ybbat <cameron@cgst.info>
2024-02-15 21:55:56 +0000

    Initial tree printing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{iter::zip, path::PathBuf};
 
 use clap::Parser;
 
@@ -17,10 +17,7 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
 
-    println!(
-        "children {:?}",
-        args.path.read_dir().unwrap().collect::<Vec<_>>()
-    );
+    print_tree(args.path, args.depth);
 }
 
 fn parse_dir(arg: &str) -> Result<PathBuf, std::io::Error> {
@@ -38,4 +35,46 @@ fn parse_dir(arg: &str) -> Result<PathBuf, std::io::Error> {
         }
         Err(e) => return Err(e),
     }
+}
+
+const TRUNK: &str = "│  ";
+const BRANCH: &str = "├──";
+const FINAL_BRANCH: &str = "└──";
+
+fn print_tree(path: PathBuf, depth: u32) {
+    println!("{}", path.display());
+    print_tree_recursive(path, depth, 0);
+}
+
+fn print_tree_recursive(path: PathBuf, max_depth: u32, cur_depth: u32) {
+    if cur_depth >= max_depth {
+        return;
+    }
+
+    let entries: Vec<_> = match path.read_dir() {
+        Ok(entries) => entries,
+        Err(_) => return,
+    }
+    .collect();
+
+    let count = entries.len();
+
+    zip(entries.iter(), 0..count).for_each(|(entry, i)| match entry {
+        Ok(entry) => {
+            if !entry.metadata().unwrap().is_dir() {
+                return;
+            }
+
+            let is_last = i == count - 1;
+            let mut prefix = if cur_depth > 0 {
+                TRUNK.repeat(cur_depth as usize)
+            } else {
+                String::new()
+            };
+            prefix += if is_last { FINAL_BRANCH } else { BRANCH };
+            println!("{}{}", prefix, entry.file_name().to_string_lossy());
+            print_tree_recursive(path.join(entry.file_name()), max_depth, cur_depth + 1)
+        }
+        Err(_) => return,
+    });
 }
