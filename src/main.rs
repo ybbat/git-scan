129346037695b4cf1aec96c562fdb1ff549133e7
ybbat <cameron@cgst.info>
2024-02-15 14:49:49 +0000

    Add required crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+/// Scans a directory tree for git repositories
+#[derive(Parser)]
+struct Cli {
+    /// Directory to start the scan from
+    #[arg(value_parser = parse_dir, default_value = ".")]
+    path: PathBuf,
+
+    /// How deep to scan
+    #[arg(short, long, default_value = "3")]
+    depth: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!(
+        "children {:?}",
+        args.path.read_dir().unwrap().collect::<Vec<_>>()
+    );
+}
+
+fn parse_dir(arg: &str) -> Result<PathBuf, std::io::Error> {
+    let path = PathBuf::from(arg);
+    match path.metadata() {
+        Ok(metadata) => {
+            if metadata.is_dir() {
+                return Ok(path.to_path_buf());
+            } else {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "Path is not a directory",
+                ));
+            }
+        }
+        Err(e) => return Err(e),
+    }
 }
